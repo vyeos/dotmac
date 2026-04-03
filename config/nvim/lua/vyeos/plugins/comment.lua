bcd75b185debd77b0042fcd06d919f1a334e8521
vyeos <rudranpatel0980@gmail.com>
2026-04-03 22:25:47 +0530

    lazy update

--- config/nvim/lua/vyeos/plugins/comment.lua
+++ config/nvim/lua/vyeos/plugins/comment.lua
@@ -1,8 +1,14 @@
 return {
-  "numToStr/Comment.nvim",
-  opts = {},
-  keys = {
-    { "<leader>/", function() require("Comment.api").toggle.linewise.current() end, mode = "n"},
-    { "<leader>/", "<Esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", mode = "v"},
-  },
+	"numToStr/Comment.nvim",
+	opts = {},
+	keys = {
+		{
+			"<leader>/",
+			function()
+				require("Comment.api").toggle.linewise.current()
+			end,
+			mode = "n",
+		},
+		{ "<leader>/", "<Plug>(comment_toggle_linewise_visual)", mode = "v" },
+	},
 }
